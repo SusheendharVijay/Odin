faf2caaeee20a6b233dc87f2da0e812d653acfe3
Sushi <susheendharvj@gmail.com>
2020-08-16 18:36:51 +0530

    made grid and hover effects

---
+++ Projects/Etch-A-Sketch/etch.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 256; i++) {
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("box");
+  newDiv.addEventListener("mouseover", (event) => {
+    event.target.classList.add("onhover");
+  });
+  newDiv.addEventListener("mouseout", removeEffect);
+  container.appendChild(newDiv);
+}
+
+function removeEffect(event) {
+  setTimeout(function () {
+    event.target.classList.remove("onhover");
+  }, 300);
+}
