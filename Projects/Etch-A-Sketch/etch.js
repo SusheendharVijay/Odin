71be22450d80f543cff61ae6e3d8b8f22cd78846
Sushi <susheendharvj@gmail.com>
2020-08-18 17:39:02 +0530

    finished etch-a-sketch

--- Projects/Etch-A-Sketch/etch.js
+++ Projects/Etch-A-Sketch/etch.js
@@ -1,17 +1,39 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 256; i++) {
-  let newDiv = document.createElement("div");
-  newDiv.classList.add("box");
-  newDiv.addEventListener("mouseover", (event) => {
-    event.target.classList.add("onhover");
-  });
-  newDiv.addEventListener("mouseout", removeEffect);
-  container.appendChild(newDiv);
+makeContainer(16, 16);
+function removeEffect(event) {
+  /* setTimeout(function () {
+    event.target.classList.remove("onhover");
+  }, 150);*/
 }
 
-function removeEffect(event) {
-  setTimeout(function () {
-    event.target.classList.remove("onhover");
-  }, 150);
+function getSquares() {
+  let sideLength = window.prompt("Enter the side length of new grid");
+  if (sideLength) {
+    removeChildren(container);
+    makeContainer(sideLength);
+    let boxSize = 960 / sideLength;
+    container.style.gridTemplateColumns = `repeat(${sideLength},${boxSize}px)`;
+    container.style.gridTemplateRows = `repeat(${sideLength},${boxSize}px)`;
+  }
+}
+
+function removeChildren(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function makeContainer(sideLength) {
+  let totalDivs = sideLength * sideLength;
+
+  for (let i = 0; i < totalDivs; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("box");
+    newDiv.addEventListener("mouseover", (event) => {
+      event.target.classList.add("onhover");
+    });
+    newDiv.addEventListener("mouseout", removeEffect);
+    container.appendChild(newDiv);
+  }
 }
