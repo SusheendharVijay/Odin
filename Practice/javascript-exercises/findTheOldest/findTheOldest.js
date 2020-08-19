2f5c75e2d76664278e874e1edbff898c80ec28b2
Sushi <susheendharvj@gmail.com>
2020-08-19 20:01:58 +0530

    finished exercises

--- Practice/javascript-exercises/findTheOldest/findTheOldest.js
+++ Practice/javascript-exercises/findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-let findTheOldest = function() {
+let findTheOldest = function (array) {
+  return array.reduce(
+    (oldest, obj) => {
+      let currentYear = new Date().getFullYear();
+      let age;
+      if (!obj.yearOfDeath) {
+        //not dead yet
+        age = currentYear - obj.yearOfBirth;
+      } else {
+        //dead
+        age = obj.yearOfDeath - obj.yearOfBirth;
+      }
 
-}
+      if (!oldest.yearOfDeath) {
+        // the oldest
+        if (age > currentYear - oldest.yearOfBirth) {
+          oldest = obj;
+        }
+      } else {
+        if (age > oldest.yearOfDeath - oldest.yearOfBirth) {
+          oldest = obj;
+        }
+      }
+      return oldest;
+    },
+    { yearOfBirth: Infinity, yearOfDeath: 0 }
+  );
+};
 
-module.exports = findTheOldest
+module.exports = findTheOldest;
