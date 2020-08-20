685ae96710318882511494b28d55ea5cbb3eab84
Sushi <susheendharvj@gmail.com>
2020-08-20 18:28:48 +0530

    started on calculator

---
+++ Projects/calculator/index.js
@@ -0,0 +1,36 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  if (num2 === 0) return "Don't divide by zero!, not cool";
+
+  return num1 / num2;
+}
+
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+      break;
+    case "-":
+      return subtract(num1, num2);
+      break;
+    case "*":
+      return multiply(num1, num2);
+      break;
+    case "/":
+      return divide(num1, num2);
+      break;
+    default:
+      return "Not a valid a operator!";
+  }
+}
