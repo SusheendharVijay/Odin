587e9324c180e13841564b81aa47ee027b3cc28b
Sushi <susheendharvj@gmail.com>
2020-08-27 14:21:44 +0530

    added calc functionality for two number operations

--- Projects/calculator/index.js
+++ Projects/calculator/index.js
@@ -37,6 +37,8 @@ function operate(num1, operator, num2) {
 
 const buttons = document.querySelector(".numberBtns");
 const operatorBtns = document.querySelector(".operatorBtns");
+const clearBtn = document.getElementById("clearBtn");
+const displayText = document.getElementById("displayText");
 for (let i = 0; i <= 9; i++) {
   let newDiv = document.createElement("div");
   newDiv.textContent = i;
@@ -44,11 +46,44 @@ for (let i = 0; i <= 9; i++) {
   buttons.appendChild(newDiv);
 }
 
-let operators = ["+", "-", "/", "."];
+let operators = ["+", "-", "*", "/"];
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < operators.length; i++) {
   let newDiv = document.createElement("div");
   newDiv.textContent = operators[i];
   newDiv.setAttribute("id", "operators");
   operatorBtns.appendChild(newDiv);
+}
+
+clearBtn.addEventListener("click", (event) => {
+  displayText.textContent = "";
+});
+const numberButtons = [...buttons.querySelectorAll("div")];
+const operatorButtons = [...operatorBtns.querySelectorAll("div")];
+const equalSign = document.querySelector(".equalSign");
+for (let number of numberButtons) {
+  number.addEventListener("click", (event) => {
+    let numVal = event.target.textContent;
+    let displayedText = displayText.textContent;
+    displayText.textContent = displayedText + numVal;
+  });
+}
+for (let operator of operatorButtons) {
+  operator.addEventListener("click", (event) => {
+    let operation = event.target.textContent;
+    let displayedText = displayText.textContent;
+    displayText.textContent = displayedText + operation;
+  });
+}
+equalSign.addEventListener("click", evaluate);
+
+function evaluate() {
+  let displayedText = displayText.textContent;
+  let literals = displayedText.split("");
+  let num1 = Number(literals[0]);
+  let num2 = Number(literals[2]);
+  let operation = displayedText[1];
+
+  let evaluated = operate(num1, operation, num2);
+  displayText.textContent = evaluated.toString();
 }
